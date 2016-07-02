e835afbae690a39a135533d9d5e7ead0c2e06847
Jakub Petrik <petrik@inloop.eu>
2016-07-02 06:56:03 +0200

    Tidy up project

--- Keychain/Keychain macOS/Keychain macOS.h
+++
@@ -1,7 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for Keychain macOS.
-FOUNDATION_EXPORT double Keychain_macOSVersionNumber;
-
-//! Project version string for Keychain macOS.
-FOUNDATION_EXPORT const unsigned char Keychain_macOSVersionString[];
