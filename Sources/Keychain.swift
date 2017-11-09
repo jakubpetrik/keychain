ad2f182091bef1bdd4b25374eeabe62a8c2139fc
Jakub Petrík <petrik@inloop.eu>
2017-11-09 06:42:21 +0100

    Migrate to Swift 4

--- Sources/Keychain.swift
+++ Sources/Keychain.swift
@@ -3,72 +3,64 @@ import Foundation
 public final class Keychain {
     private let identifier: String
     private lazy var values: NSMutableDictionary = {
-        guard let data = self.load()
-            , values = NSKeyedUnarchiver.unarchiveObject(with: data) as? NSMutableDictionary else {
-                return NSMutableDictionary()
-        }
+        guard let data = loadData()
+            , let values = NSKeyedUnarchiver.unarchiveObject(with: data) as? NSMutableDictionary
+            else { return NSMutableDictionary() }
         return values
     }()
 
     private func save(data: Data) -> Bool {
-        let query = [
-              kSecClass as String       : kSecClassGenericPassword as String
-            , kSecAttrAccount as String : identifier
-            , kSecValueData as String   : data
-        ]
+        let query =
+            [ kSecClass: kSecClassGenericPassword
+            , kSecAttrAccount: identifier
+            , kSecValueData: data
+            ] as CFDictionary
 
-        SecItemDelete(query as CFDictionary)
-
-        let status = SecItemAdd(query as CFDictionary, nil)
-
+        SecItemDelete(query)
+        let status = SecItemAdd(query, nil)
         return status == noErr
     }
 
-    private func load() -> Data? {
-        let query = [
-              kSecClass as String       : kSecClassGenericPassword
-            , kSecAttrAccount as String : identifier
-            , kSecReturnData as String  : kCFBooleanTrue
-            , kSecMatchLimit as String  : kSecMatchLimitOne
-        ]
+    private func loadData() -> Data? {
+        let query =
+            [ kSecClass: kSecClassGenericPassword
+            , kSecAttrAccount: identifier
+            , kSecReturnData: kCFBooleanTrue
+            , kSecMatchLimit: kSecMatchLimitOne
+            ] as CFDictionary
 
-        let dataTypeRef = UnsafeMutablePointer<AnyObject?>(allocatingCapacity: 1)
-
+        let dataTypeRef = UnsafeMutablePointer<AnyObject?>.allocate(capacity: 1)
         let status = SecItemCopyMatching(query, dataTypeRef)
-
-        guard let data = dataTypeRef.pointee as? Data where status == noErr else { return nil }
+        guard let data = dataTypeRef.pointee as? Data, status == noErr else { return nil }
         return data
     }
 
-    private func delete() -> Bool {
-        let query = [
-              kSecClass as String       : kSecClassGenericPassword
-            , kSecAttrAccount as String : identifier
-        ]
+    private func delete() {
+        let query =
+            [ kSecClass: kSecClassGenericPassword
+            , kSecAttrAccount: identifier
+            ] as CFDictionary
 
-        let status = SecItemDelete(query as CFDictionary)
-        
-        return status == noErr
+        SecItemDelete(query)
     }
 
     public init(identifier: String) {
         self.identifier = identifier
     }
 
-    public subscript(aKey: NSCopying) -> AnyObject? {
+    public subscript<T: AnyObject>(aKey: NSCopying) -> T? {
         get { return object(forKey: aKey) }
         set { _ = setObject(newValue, forKey: aKey) }
     }
 
-    public func object<T: AnyObject>(forKey aKey: NSCopying) -> T? {
+    private func object<T: AnyObject>(forKey aKey: NSCopying) -> T? {
         return values.object(forKey: aKey) as? T
     }
 
-    public func setObject(_ anObject: AnyObject?, forKey aKey: NSCopying) -> Bool {
+    private func setObject(_ anObject: AnyObject?, forKey aKey: NSCopying) -> Bool {
         if let objectToSet = anObject {
             values.setObject(objectToSet, forKey: aKey)
-        }
-        else {
+        } else {
             values.removeObject(forKey: aKey)
         }
         let archivedData = NSKeyedArchiver.archivedData(withRootObject: values)
@@ -76,7 +68,7 @@ public final class Keychain {
     }
 
     public func clear() {
-        _ = delete()
+        delete()
         values = NSMutableDictionary()
     }
 }
